a535bc4409ecb5ce8b424a3e3da19cf87134d769
lozert <90326151+lozert@users.noreply.github.com>
2023-02-17 14:20:11 +0300

    Add files via upload

---
+++ exp_9.java
@@ -0,0 +1,12 @@
+package Lab_8_odnomernie_massiv;
+
+public class exp_9 {
+    public static void main(String args[]){
+        int n = 4, natchal_i = 0 , natchal_j = 0 , finish_i = n-1 , finish_j = n-1,
+        count = 0;
+
+        for(int i = finish_i; i != finish_i-1;i--){
+
+        }
+    }
+}
